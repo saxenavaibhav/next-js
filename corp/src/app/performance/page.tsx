7b9aea47077edcf963f651232aaab3bf9521302a
Vaibhav Saxena <reachvaibhavsaxena@gmail.com>
2024-01-03 20:17:33 +0530

    Adding Links

--- corp/src/app/performance/page.tsx
+++ corp/src/app/performance/page.tsx
@@ -1,3 +1,3 @@
-export default function Home() {
+export default function PerformancePage() {
   return <div>Performance page</div>;
 }
--- corp/src/app/scale/page.tsx
+++ corp/src/app/scale/page.tsx
@@ -1,3 +1,3 @@
-export default function Home() {
+export default function ScalePage() {
   return <div>Scale page</div>;
 }
